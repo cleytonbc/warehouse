31dd467436c0a59341eb29d53ddfcd4ee44f4765
Cleyton <cleytonbc@hotmail.com>
2020-09-11 18:37:59 -0300

    Adicionando campo - Excercicio

--- src/main/java/br/gov/rj/faeterj/estoque/model/Produto.java
+++ src/main/java/br/gov/rj/faeterj/estoque/model/Produto.java
@@ -1,12 +1,20 @@
 package br.gov.rj.faeterj.estoque.model;
 
+import javax.validation.constraints.Size;
+
 import org.hibernate.validator.constraints.NotBlank;
 
 public class Produto {
 
 	@NotBlank
 	private String sku;
+	
+	@NotBlank
 	private String nome;
+	
+	@NotBlank
+	@Size (max = 50)
+	private String descricao;
 
 	
 	public String getSku() {
@@ -24,5 +32,15 @@ public class Produto {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
 	
 }
